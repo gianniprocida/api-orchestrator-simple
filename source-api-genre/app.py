ed794200e2253cf485967cb7aacdf036f580589a
gianniprocida <giannyprocida@gmail.com>
2024-02-23 16:53:25 +0100

    updated source-api-genre

--- source-api-genre/app.py
+++ source-api-genre/app.py
@@ -5,56 +5,46 @@ import json
 import time
 import datetime
 from database_utils import getConn
-import os,base64
+import os
+import base64
 import logging
 from time import gmtime
 import base64
 
 
-
+host = os.environ.get('MYSQL_HOST'),
+user = os.environ.get('MYSQL_USER'),
+password = os.environ.get('MYSQL_ROOT_PASSWORD'),
+db = os.environ.get('MYSQL_DATABASE')
 
 app = Flask(__name__)
 
 
-
-@app.route('/')
-def display_menu():
-    return render_template('menu.html')
-
 @app.route('/filter/genre', methods=['GET', 'POST'])
-def filter_by_id_route():
+def filter_genre():
     if request.method == 'POST':
         # Validate myid
         try:
             myid = int(request.form['myid'])
-            if 1 <= myid <= 10:
+            if 1 <= myid <= 12:
                 print("Valid request..")
             else:
-                print("ID must be between 1 and 10")
-        
+                print("ID must be between 1 and 12")
+
         except ValueError:
             print("ID must be a valid integer..")
 
-
-        host=os.environ.get('MYSQL_HOST'),
-        user=os.environ.get('MYSQL_USER'),
-        password=os.environ.get('MYSQL_ROOT_PASSWORD'),
-        db=os.environ.get('MYSQL_DATABASE')
-        cnx = getConn(host,user,password,db)
+        cnx = getConn(host, user, password, db)
         cur = cnx.cursor()
         query = """select genre_name from genre where id=%s;"""
-        cur.execute(query,(myid,))
+        cur.execute(query, (myid,))
         rows = cur.fetchall()
         dict_data = []
         for a in rows:
-           dict_data.append({'genre':a})
-       
+            dict_data.append({'genre': a})
+
         return jsonify(dict_data)
     return render_template('filter_genre.html')
 
 
-# @app.route('/quit')
-# def quit():
-#     return "Goodbye!"
-
-app.run(debug=True)
+app.run(host="0.0.0.0")
