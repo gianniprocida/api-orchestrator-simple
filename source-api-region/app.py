6fb86f5c102b087dff53e2f2840074cdf3e0e17c
gianniprocida <giannyprocida@gmail.com>
2024-02-23 16:54:22 +0100

    updated source-api-region

--- source-api-region/app.py
+++ source-api-region/app.py
@@ -5,57 +5,52 @@ import json
 import time
 import datetime
 from database_utils import getConn
-import os,base64
+import os
+import base64
 import logging
 from time import gmtime
 import base64
 
 
-
+host = os.environ.get('MYSQL_HOST'),
+user = os.environ.get('MYSQL_USER'),
+password = os.environ.get('MYSQL_ROOT_PASSWORD'),
+db = os.environ.get('MYSQL_DATABASE')
 
 app = Flask(__name__)
-
 
 
 @app.route('/')
 def display_menu():
     return render_template('menu.html')
 
+
 @app.route('/filter/region', methods=['GET', 'POST'])
-def filter_by_id_route():
+def filter_by_region():
     if request.method == 'POST':
-       
+
         # Validate myid
         try:
             myid = int(request.form['myid'])
-            if 1 <= myid <= 10:
+            if 1 <= myid <= 4:
                 print("Valid request..")
             else:
-                print("ID must be between 1 and 10")
-        
+                print("ID must be between 1 and 4")
+
         except ValueError:
             print("Rating must be a valid integer..")
 
-
-        host=os.environ.get('MYSQL_HOST'),
-        user=os.environ.get('MYSQL_USER'),
-        password=os.environ.get('MYSQL_ROOT_PASSWORD'),
-        db=os.environ.get('MYSQL_DATABASE')
-        cnx = getConn(host,user,password,db)
+        cnx = getConn(host, user, password, db)
         cur = cnx.cursor()
         query = """select region_name from region where id=%s;"""
-        cur.execute(query,(myid,))
+        cur.execute(query, (myid,))
         rows = cur.fetchall()
         dict_data = []
         for a in rows:
-           dict_data.append({'region_name':a})
-       
+            dict_data.append({'region_name': a})
+
         return jsonify(dict_data)
     return render_template('filter_region.html')
 
-
-# @app.route('/quit')
-# def quit():
-#     return "Goodbye!"
 
 app.run(host="0.0.0.0")
